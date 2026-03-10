12bc6e8b58dd5ffe1b6654c5a0e05a68804b2a4e
vikramsandhu90341 <vikramsandhu90341@gmail.com>
2026-03-10 10:46:04 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE = "cos-v999";
+
+self.addEventListener("install", e => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request));
+});
